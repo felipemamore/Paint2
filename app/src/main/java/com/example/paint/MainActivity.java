74f9af7239fcebfdb68ec36c7135dca003b63a6e
felipemamore <felipemamore@gmail.com>
2024-10-07 22:10:08 -0300

    Atualizando Paint

--- app/src/main/java/com/example/paint/MainActivity.java
+++ app/src/main/java/com/example/paint/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.paint;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.skydoves.colorpickerview.ColorEnvelope;
+import com.skydoves.colorpickerview.ColorPickerDialog;
+import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener;
 
 public class MainActivity extends AppCompatActivity {
+    SimplePaint simplePaint;
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        simplePaint=findViewById(R.id.simplePaint);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new ColorPickerDialog.Builder(MainActivity.this)
+                        .setTitle("ColorPicker Dialog")
+                        .setPreferenceName("MyColorPickerDialog")
+                        .setPositiveButton(getString(R.string.confirm),
+                                new ColorEnvelopeListener() {
+                                    @Override
+                                    public void onColorSelected(ColorEnvelope envelope, boolean fromUser) {
+                                        setLayoutColor(envelope);
+                                    }
+                                })
+                        .setNegativeButton(getString(R.string.cancel),
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        dialogInterface.dismiss();
+                                    }
+                                })
+                        .attachAlphaSlideBar(true) // the default value is true.
+                        .attachBrightnessSlideBar(true)  // the default value is true.
+                        .setBottomSpace(12) // set a bottom space between the last slidebar and buttons.
+                        .show();
+
+            }
+        });
+
+    }
+
+    public void setLayoutColor(ColorEnvelope envelope) {
+        simplePaint.mPaint.setColor(envelope.getColor());
     }
 }
--- app/src/main/java/com/example/paint/SimplePaint.java
+++ app/src/main/java/com/example/paint/SimplePaint.java
@@ -56,5 +56,7 @@ public class SimplePaint extends View {
         invalidate();
 
         return super.onTouchEvent(event);
+
     }
+
 }
